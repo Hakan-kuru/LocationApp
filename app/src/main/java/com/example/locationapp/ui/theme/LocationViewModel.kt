f8e459795328e7f324f58d30bffcdca9094521f2
Hakan-kuru <hakan.k3272@gmail.com>
2025-02-12 17:32:40 +0300

    .

---
+++ app/src/main/java/com/example/locationapp/ui/theme/LocationViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.locationapp.ui.theme
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class LocationViewModel: ViewModel()  {
+
+    private val _locationData = mutableStateOf<LocationData?>(null)
+    val locationData: State<LocationData?> = _locationData
+
+    fun updateLocation(newLocation: LocationData){
+        _locationData.value = newLocation
+    }
+}
